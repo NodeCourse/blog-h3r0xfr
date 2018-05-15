b54eb3471f7fca0e92f39283c043df418d58b3f0
h3r0xfr <adrien.cabaud@numericable.fr>
2018-05-15 23:55:48 +0200

    implements image upload

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const Sequelize = require('sequelize');
 const dateFormat = require('dateformat');
-const bodyParser = require('body-parser');
+const formidable = require('formidable');
+const fs = require('fs');
 const express = require('express');
 const app = express();
 
@@ -13,16 +14,18 @@ const Post = db.define('post', {
     title: { type: Sequelize.STRING },
     description: { type: Sequelize.STRING },
     content: { type: Sequelize.STRING },
-    author: { type: Sequelize.STRING },
-    category: { type: Sequelize.STRING }
+    photo: { type: Sequelize.STRING }
 });
 
 app.set('view engine', 'pug');
 app.set("views", "public/views");
-
 app.use(express.static("public"));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+
+function showError(res, view, message) {
+    res.render(view, {
+        error: message
+    });
+}
 
 app.get('/', (req, res) => {
     Post.findAll().then((data) => {
@@ -51,31 +54,47 @@ app.get('/post/new', (req, res) => {
 });
 
 app.post('/post/new', (req, res) => {
-    let pTitle = req.body.title;
-    let pDescription = req.body.description;
-    let pContent = req.body.content;
-    let pAuthor = req.body.author;
-    let pCategory = req.body.category;
 
-    if(pTitle !== '' && pDescription !== '' && pContent !== '' && pAuthor != '' && pCategory != '') {
-        Post
-            .sync()
-            .then(() => {
-                Post.create({
-                    title: pTitle,
-                    description: pDescription,
-                    content: pContent,
-                    author: pAuthor,
-                    category: pCategory
-                }).then((result) => {
-                    res.redirect('/post/' + result.id);
-                });
+    let form = new formidable.IncomingForm();
+
+    form.parse(req, function (err, fields, files) {
+        let pTitle = fields.title;
+        let pDescription = fields.description;
+        let pContent = fields.content;
+        let pPhoto = files.photo;
+
+        if(pTitle !== '' && pDescription !== '' && pContent !== '' && pPhoto !== '')
+        {
+            let oldpath = pPhoto.path;
+            let newpath = './public/images/uploads/' + pPhoto.name;
+
+            if(!pPhoto.name.match(/\.(jpg|jpeg|png)$/i)) {
+                return showError(res, 'new', 'Les extensions acceptées pour la photo sont : jpg, jpeg, png');
+            }
+
+            fs.rename(oldpath, newpath, function (err) {
+                if (err) {
+                    console.log(err);
+                    return showError(res, 'new', 'Une erreur est survenue lors de l\'envoi du fichier.');
+                }
             });
-    } else {
-        res.render('/post/new', {
-            error: 'Vous devez compléter tous les champs.'
-        });
-    }
+
+            Post
+                .sync()
+                .then(() => {
+                    Post.create({
+                        title: pTitle,
+                        description: pDescription,
+                        content: pContent,
+                        photo: pPhoto.name
+                    }).then((result) => {
+                        res.redirect('/post/' + result.id);
+                    });
+                });
+        }
+        else
+            return showError(res, 'new', 'Vous devez compléter tous les champs.');
+    });
 });
 
 app.listen(3000);
